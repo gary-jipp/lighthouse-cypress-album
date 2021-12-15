edf41b42a5410e3758bc8d3f3ceeeb8cd0e03489
Gary J <github@accu.ca>
2021-12-14 21:20:48 -0800

    Add alias

--- cypress/integration/3-typing.spec.js
+++ cypress/integration/3-typing.spec.js
@@ -3,23 +3,23 @@ describe('tests for the input field', () => {
 
   beforeEach(() => {
     cy.visit('/');
+    cy.get('.search__form')
+    .find('input.radius')
+    .as('inputField');
   });
 
   it('can type into the search input field', () => {
-    cy.get('.search__form')
-      .find('input.radius')
+    cy.get('@inputField')
       .type('Queen', {delay:250})
   });
 
   it('can handle backspaces', () => {
-    cy.get('.search__form')
-      .find('input.radius')
+    cy.get('@inputField')
       .type('Beee{backspace}ge{backspace}{backspace}Gees', {delay:250})
   });
 
   it('can handle curly braces', () => {
-    cy.get('.search__form')
-      .find('input.radius')
+    cy.get('@inputField')
       .type('Beee{backspace}ge{backspace}{backspace}Gees', {delay:250, parseSpecialCharSequences:false })
   });
 
