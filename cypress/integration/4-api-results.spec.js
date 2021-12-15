af6e254200ca17eba0573f199b9942a5cc822946
Gary J <github@accu.ca>
2021-12-14 22:07:16 -0800

    Add mock data & test

---
+++ cypress/integration/4-api-results.spec.js
@@ -0,0 +1,28 @@
+/* eslint-disable no-undef */
+describe('tests for the input field', () => {
+
+  it('can type into the search input field', () => {
+    cy.visit('/')
+      .intercept("GET", "**/search*", { fixture: "itunes.json" })
+      .as("itunesResults");
+
+    cy.get('.search__form')
+      .find('input.radius')
+      .type("Daft Punk", { delay: 200 });
+
+    cy.get(".spinner")
+      .should("be.visible");
+
+    cy.wait("@itunesResults")
+      .get("main")
+      .contains("Random Access");
+
+    cy.get("#Explicit")
+      .uncheck();
+
+    cy.get("article.album")
+      .should("not.contain", "Daft Club");
+
+  });
+
+});
