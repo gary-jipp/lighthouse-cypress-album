7856c30a2884cd2f88e5c198e00764a0acfca546
Gary J <github@accu.ca>
2021-12-14 21:08:31 -0800

    Add

---
+++ cypress/integration/3-typing.spec.js
@@ -0,0 +1,20 @@
+/* eslint-disable no-undef */
+describe('tests for the input field', () => {
+
+  beforeEach(() => {
+    cy.visit('/');
+  });
+
+  it('can type into the search input field', () => {
+    cy.get('.search__form')
+      .find('input.radius')
+      .type('Queen', {delay:250})
+  });
+
+  it('can handle backspaces', () => {
+    cy.get('.search__form')
+      .find('input.radius')
+      .type('Beee{backspace}ge{backspace}{backspace}Gees', {delay:250})
+  });
+
+});
